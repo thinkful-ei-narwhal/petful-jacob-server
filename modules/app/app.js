5ff5fe4ecb0d643c77fc801f9a861abe8e5d03e5
Jacob Flaxman <jacobflaxman1@gmail.com>
2020-06-19 14:21:29 -0700

    cors problems

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const cors = require('cors')
-
+const config = require('../../config')
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: config.CLIENT_ORIGIN
+}))
 
 app.use('/people', require('../people/people.router'))
 app.use('/pets', require('../pets/pets.router'))
